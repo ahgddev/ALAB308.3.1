67e7515c6c188830b18b5353f2db68feb80e7410
ahgddev <ahgddev@gmail.com>
2024-01-17 19:13:45 -0700

    Finished part 2! I figured out the problem by myself!

--- lab.js
+++ lab.js
@@ -15,21 +15,31 @@
 // }
 
 // Part 2: Prime Time
-// I need help with this.
-let num_2 = 366;
-let numstring = String(num_2);
-let notprime = ["0", "2", "4", "5", "6", "8"];
-let foundprime = false;
 
-while (foundprime == false) {
-    // debugger
-    for(checknum in notprime) {
-        console.log(numstring, checknum);
-        if (numstring.endsWith(checknum)) {
-            num_2++;
-            continue;
+let num_2 = 368;
+
+function isprime (anumber) {
+    let primecheck = 2;
+    while (primecheck <= num_2/2) {
+        if (num_2 % primecheck != 0) {
+            primecheck += 1;
+        } else {
+            return isprime(num_2+=1)
         }
     }
-    foundprime = true;
     console.log(num_2)
 }
+isprime(num_2)
+
+
+// INPUT n
+// i = 2
+// answer = prime
+// WHILE i <= n / 2
+//     rem = n % i
+//     IF rem is not equal to 0
+//         i = i + 1
+//     ELSE
+//         answer = not prime
+//         END WHILE LOOP
+// OUTPUT answer
